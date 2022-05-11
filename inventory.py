ec38f832e8bc7ccbd7b6cbd3c41be7d9d26a04ee
Wissens <wissenschaftecht@gmail.com>
2022-05-11 17:00:31 -0500

    Initial commit

--- inventory.py
+++ inventory.py
@@ -31,30 +31,30 @@ class Inventory:
         connection.close()
 
     def remove(user, item):
-    connection = sqlite3.connect('database.db')
-    records = connection.cursor()
+        connection = sqlite3.connect('database.db')
+        records = connection.cursor()
 
-    bag = f'inventory_{user}'
+        bag = f'inventory_{user}'
 
-    try:
-        records.execute(f"""CREATE TABLE {bag} (
-            item_id INTEGER,
-            amount INTEGER
-        )""")
-    except sqlite3.OperationalError: pass
-    
-    records.execute(f"SELECT amount FROM {bag} WHERE item_id = {item}")
-    check = records.fetchone()
+        try:
+            records.execute(f"""CREATE TABLE {bag} (
+                item_id INTEGER,
+                amount INTEGER
+            )""")
+        except sqlite3.OperationalError: pass
+        
+        records.execute(f"SELECT amount FROM {bag} WHERE item_id = {item}")
+        check = records.fetchone()
 
-    if(check):
-        if(check[0]<=1):
-            records.execute(f"DELETE FROM {bag} WHERE item_id = {item}")
+        if(check):
+            if(check[0]<=1):
+                records.execute(f"DELETE FROM {bag} WHERE item_id = {item}")
+            else:
+                records.execute(f"""UPDATE {bag} SET amount = amount - 1
+                WHERE item_id = {item}""")
+            connection.commit()
+            connection.close()
+            return True
         else:
-            records.execute(f"""UPDATE {bag} SET amount = amount - 1
-            WHERE item_id = {item}""")
-        connection.commit()
-        connection.close()
-        return True
-    else:
-        connection.close()
-        return False
+            connection.close()
+            return False
